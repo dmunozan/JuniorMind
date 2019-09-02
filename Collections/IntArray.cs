bab4131a1af146a6fbbabbb49ef65db81538e207
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-02 18:15:37 +0300

    Created class. Added constructor and Contains method

---
+++ Collections/IntArray.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Collections
+{
+    public class IntArray
+    {
+        readonly int[] intArray;
+
+        public IntArray()
+        {
+            this.intArray = new int[0];
+        }
+
+        public bool Contains(int element)
+        {
+            Console.WriteLine(intArray + "" + element);
+            return false;
+        }
+    }
+}
