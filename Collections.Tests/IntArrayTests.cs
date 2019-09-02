2e2de3c0ef193235cbe5fe0f3f0c0ddb7fd6046d
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-02 18:14:58 +0300

    Created class and added test ContainsWhenNumberAndArrayIsEmptyShouldReturnFalse

---
+++ Collections.Tests/IntArrayTests.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace Collections.Tests
+{
+    public class IntArrayTests
+    {
+        [Fact]
+        public void ContainsWhenNumberAndArrayIsEmptyShouldReturnFalse()
+        {
+            IntArray arrayTest = new IntArray();
+
+            Assert.False(arrayTest.Contains(5));
+        }
+    }
+}
