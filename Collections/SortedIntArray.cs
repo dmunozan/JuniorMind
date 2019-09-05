bb70030e80a43a7ab74aa7d1358e96a9a767aa2f
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-05 19:23:38 +0300

    Created class. Added constructor and Add method

---
+++ Collections/SortedIntArray.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Collections
+{
+    public class SortedIntArray : IntArray
+    {
+        public SortedIntArray() : base()
+        {
+        }
+
+        public override void Add(int element)
+        {
+            int position = 0;
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (this[i] >= element)
+                {
+                    position = i;
+                    break;
+                }
+            }
+
+            if (position == this.Count)
+            {
+                base.Add(element);
+            }
+            else
+            {
+                Insert(position, element);
+            }
+        }
+    }
+}
