61c6d95f7b99ac3057f8df3e661ab5adbd7320e8
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-05 20:23:47 +0300

    Added tests InsertWhenPosition2Number4ShouldInsertNumber - already passing InsertWhenPosition2Number5ShouldInsertNumber - already passing InsertWhenPosition2Number6ShouldDoNothing - already passing InsertWhenPosition1Number4ShouldDoNothing

--- Collections.Tests/SortedIntArrayTests.cs
+++ Collections.Tests/SortedIntArrayTests.cs
@@ -54,5 +54,71 @@ namespace Collections.Tests
             Assert.Equal(2, arrayTest[0]);
             Assert.Equal(5, arrayTest[1]);
         }
+
+        [Fact]
+        public void InsertWhenPosition2Number4ShouldInsertNumber()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+            arrayTest.Add(3);
+
+            arrayTest.Insert(2, 4);
+
+            Assert.Equal(4, arrayTest.Count);
+            Assert.Equal(2, arrayTest[0]);
+            Assert.Equal(3, arrayTest[1]);
+            Assert.Equal(4, arrayTest[2]);
+            Assert.Equal(5, arrayTest[3]);
+        }
+
+        [Fact]
+        public void InsertWhenPosition2Number5ShouldInsertNumber()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+            arrayTest.Add(3);
+
+            arrayTest.Insert(2, 5);
+
+            Assert.Equal(4, arrayTest.Count);
+            Assert.Equal(2, arrayTest[0]);
+            Assert.Equal(3, arrayTest[1]);
+            Assert.Equal(5, arrayTest[2]);
+            Assert.Equal(5, arrayTest[3]);
+        }
+
+        [Fact]
+        public void InsertWhenPosition2Number6ShouldDoNothing()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+            arrayTest.Add(3);
+
+            arrayTest.Insert(2, 6);
+
+            Assert.Equal(3, arrayTest.Count);
+            Assert.Equal(2, arrayTest[0]);
+            Assert.Equal(3, arrayTest[1]);
+            Assert.Equal(5, arrayTest[2]);
+        }
+
+        [Fact]
+        public void InsertWhenPosition1Number4ShouldDoNothing()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(6);
+            arrayTest.Add(7);
+            arrayTest.Add(5);
+
+            arrayTest.Insert(1, 4);
+
+            Assert.Equal(3, arrayTest.Count);
+            Assert.Equal(5, arrayTest[0]);
+            Assert.Equal(6, arrayTest[1]);
+            Assert.Equal(7, arrayTest[2]);
+        }
     }
 }
