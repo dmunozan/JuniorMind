ebdcb8420e505a2376ab93f026784fc29338f975
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-05 19:48:43 +0300

    Added test InsertWhenPosition0Number1ShouldReturnCount3AndBeSorted - Already passed InsertWhenPosition0Number3ShouldDoNothing

--- Collections.Tests/SortedIntArrayTests.cs
+++ Collections.Tests/SortedIntArrayTests.cs
@@ -25,5 +25,34 @@ namespace Collections.Tests
             Assert.Equal(2, arrayTest[0]);
             Assert.Equal(5, arrayTest[1]);
         }
+
+        [Fact]
+        public void InsertWhenPosition0Number1ShouldReturnCount3AndBeSorted()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+
+            arrayTest.Insert(0, 1);
+
+            Assert.Equal(3, arrayTest.Count);
+            Assert.Equal(1, arrayTest[0]);
+            Assert.Equal(2, arrayTest[1]);
+            Assert.Equal(5, arrayTest[2]);
+        }
+
+        [Fact]
+        public void InsertWhenPosition0Number3ShouldDoNothing()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+
+            arrayTest.Insert(0, 3);
+
+            Assert.Equal(2, arrayTest.Count);
+            Assert.Equal(2, arrayTest[0]);
+            Assert.Equal(5, arrayTest[1]);
+        }
     }
 }
