dae7a878da6d920e980f171b5a9175f2cd9827f0
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-05 19:23:01 +0300

    Created class. Added tests AddWhenEmptyShouldReturnCount1 AddWhenOneNumberShouldReturnCount2AndBeSorted

---
+++ Collections.Tests/SortedIntArrayTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace Collections.Tests
+{
+    public class SortedIntArrayTests
+    {
+        [Fact]
+        public void AddWhenEmptyShouldReturnCount1()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+
+            Assert.Equal(1, arrayTest.Count);
+        }
+
+        [Fact]
+        public void AddWhenOneNumberShouldReturnCount2AndBeSorted()
+        {
+            SortedIntArray arrayTest = new SortedIntArray();
+            arrayTest.Add(5);
+            arrayTest.Add(2);
+
+            Assert.Equal(2, arrayTest.Count);
+            Assert.Equal(2, arrayTest[0]);
+            Assert.Equal(5, arrayTest[1]);
+        }
+    }
+}
