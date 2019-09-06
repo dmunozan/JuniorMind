de13ab64b89d524be73aacab76b5177ca7c6f23a
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-06 19:24:40 +0300

    Created ObjectArray Added member, constructor, propertie, indexer, Add method and EnsureCapacity method

---
+++ Collections/ObjectArray.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Collections
+{
+    public class ObjectArray
+    {
+        object[] objectArray;
+
+        public ObjectArray()
+        {
+            const int BaseSize = 4;
+            this.objectArray = new object[BaseSize];
+            this.Count = 0;
+        }
+
+        public int Count { get; private set; }
+
+        public object this[int index]
+        {
+            get => this.objectArray[index];
+            set
+            {
+                if (index < 0 || index >= this.Count)
+                {
+                    return;
+                }
+
+                this.objectArray[index] = value;
+            }
+        }
+
+        public void Add(object element)
+        {
+            this.EnsureCapacity();
+
+            this.objectArray[this.Count] = element;
+            this.Count++;
+        }
+
+        private void EnsureCapacity()
+        {
+            const int Double = 2;
+
+            if (this.Count < this.objectArray.Length)
+            {
+                return;
+            }
+
+            Array.Resize(ref this.objectArray, this.objectArray.Length * Double);
+        }
+    }
+}
