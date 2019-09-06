3787c06d039ccf804345a01ab61f6f8ea8afbd98
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-06 19:05:14 +0300

    Created class. Added tests CountWhenEmptyArrayShouldReturn0 AddWhenEmptyArrayShouldReturnCount1

---
+++ Collections.Tests/ObjectArrayTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+
+namespace Collections.Tests
+{
+    public class ObjectArrayTests
+    {
+        [Fact]
+        public void CountWhenEmptyArrayShouldReturn0()
+        {
+            ObjectArray arrayTest = new ObjectArray();
+
+            Assert.Equal(0, arrayTest.Count);
+        }
+
+        [Fact]
+        public void AddWhenEmptyArrayShouldReturnCount1()
+        {
+            ObjectArray arrayTest = new ObjectArray();
+            arrayTest.Add(4);
+
+            Assert.Equal(1, arrayTest.Count);
+        }
+    }
+}
