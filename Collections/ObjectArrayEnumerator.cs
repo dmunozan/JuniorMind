ca72634f80ea8ffddcacb2686e3d907957ca85a4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-08 20:37:59 +0300

    Modified class Added constructor and class members

--- Collections/ObjectArrayEnumerator.cs
+++ Collections/ObjectArrayEnumerator.cs
@@ -5,6 +5,15 @@ namespace Collections
 {
     public class ObjectArrayEnumerator : IEnumerator
     {
+        ObjectArrayCollection objectArrayCollection;
+        int index;
+
+        public ObjectArrayEnumerator(ObjectArrayCollection objectArrayCollection)
+        {
+            this.objectArrayCollection = objectArrayCollection;
+            index = -1;
+        }
+
         public object Current => throw new NotImplementedException();
 
         public bool MoveNext()
