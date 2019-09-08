2195ced694510c6dba847dc6bf6973869d38e82c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-08 21:10:43 +0300

    Created class and added test CurrentWhenInitialPositionShouldReturnNull

---
+++ Collections.Tests/ObjectArrayEnumeratorTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Xunit;
+
+namespace Collections.Tests
+{
+    public class ObjectArrayEnumeratorTests
+    {
+        [Fact]
+        public void CurrentWhenInitialPositionShouldReturnNull()
+        {
+            ObjectArrayEnumerator arrayEnumerator = new ObjectArrayEnumerator();
+
+            Assert.Null(arrayEnumerator.Current);
+        }
+    }
+}
