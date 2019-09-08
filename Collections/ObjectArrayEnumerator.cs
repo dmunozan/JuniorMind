28833a30b571214db74b4327713e878a9a0717bf
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-08 19:48:47 +0300

    Created class

---
+++ Collections/ObjectArrayEnumerator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace Collections
+{
+    public class ObjectArrayEnumerator : IEnumerator
+    {
+        public object Current => throw new NotImplementedException();
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
