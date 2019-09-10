f3c9118f0455170dbe7744d7871205bae74c8c5d
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-10 18:00:54 +0300

    Added test InsertWhenPosition0ShouldReturnElementOnIndex0

--- Collections.Tests/ListTests.cs
+++ Collections.Tests/ListTests.cs
@@ -82,5 +82,22 @@ namespace Collections.Tests
 
             Assert.False(listTest.Contains("test"));
         }
+
+        [Fact]
+        public void InsertWhenPosition0ShouldReturnElementOnIndex0()
+        {
+            List<int> listTest = new List<int>();
+            listTest.Add(1);
+            listTest.Add(2);
+            listTest.Add(3);
+
+            listTest.Insert(0, 4);
+
+            Assert.Equal(4, listTest.Count);
+            Assert.Equal(4, listTest[0]);
+            Assert.Equal(1, listTest[1]);
+            Assert.Equal(2, listTest[2]);
+            Assert.Equal(3, listTest[3]);
+        }
     }
 }
