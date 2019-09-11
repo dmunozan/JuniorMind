10d235f9c3ad71418c7f0722eb2a85b0638e6a12
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-11 14:58:34 +0300

    Added test InsertWhenPosition0Number3ShouldDoNothing

--- Collections.Tests/SortedListTests.cs
+++ Collections.Tests/SortedListTests.cs
@@ -40,5 +40,19 @@ namespace Collections.Tests
             Assert.Equal('b', listTest[1]);
             Assert.Equal('d', listTest[2]);
         }
+
+        [Fact]
+        public void InsertWhenPosition0Number3ShouldDoNothing()
+        {
+            SortedList<int> listTest = new SortedList<int>();
+            listTest.Add(5);
+            listTest.Add(2);
+
+            listTest.Insert(0, 3);
+
+            Assert.Equal(2, listTest.Count);
+            Assert.Equal(2, listTest[0]);
+            Assert.Equal(5, listTest[1]);
+        }
     }
 }
