ae7c0663a3e33ee1fb8f0489fd6f69e1e90beea8
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-11 14:21:57 +0300

    Added Add method override

--- Collections/SortedList.cs
+++ Collections/SortedList.cs
@@ -9,5 +9,28 @@ namespace Collections
         public SortedList() : base()
         {
         }
+
+        public override void Add(T element)
+        {
+            int position = 0;
+            for (int i = 0; i < this.Count; i++)
+            {
+                if (element.CompareTo(this[i]) < 0)
+                {
+                    break;
+                }
+
+                position++;
+            }
+
+            if (position == this.Count)
+            {
+                base.Add(element);
+            }
+            else
+            {
+                Insert(position, element);
+            }
+        }
     }
 }
