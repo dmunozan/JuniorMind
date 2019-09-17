c829ed565676f3402097edf9a324020d12c120a4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-17 19:12:20 +0300

    Created class, added constructor and property Value

---
+++ Collections/Node.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Collections
+{
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; }
+    }
+}
