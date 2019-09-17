59926af6fa0eaa7e712553a7797a11ea90e28e60
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-17 20:02:43 +0300

    Added Last property and initialization on the constructor

--- Collections/SingleLinkedList.cs
+++ Collections/SingleLinkedList.cs
@@ -6,10 +6,13 @@
         {
             Count = 0;
             First = null;
+            Last = null;
         }
 
         public int Count { get; }
 
         public Node<T> First { get; }
+
+        public Node<T> Last { get; }
     }
 }
