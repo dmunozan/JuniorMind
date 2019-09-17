3f43e1400f2ebf23e7f6ab7ed0cf730fc3164df4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-17 19:31:17 +0300

    Added test NextNodeWhenNoNodeShouldReturnNull

--- Collections.Tests/NodeTests.cs
+++ Collections.Tests/NodeTests.cs
@@ -12,5 +12,13 @@ namespace Collections.Tests
 
             Assert.Equal(0, testNode.Value);
         }
+
+        [Fact]
+        public void NextNodeWhenNoNodeShouldReturnNull()
+        {
+            Node<int> testNode = new Node<int>(0);
+
+            Assert.Null(testNode.NextNode);
+        }
     }
 }
