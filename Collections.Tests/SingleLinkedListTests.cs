d11cd3aceb4742cf984022e575357664efb8601f
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-17 19:54:50 +0300

    Created class and added test CountWhenEmptyListShouldReturn0

---
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace Collections.Tests
+{
+    public class SingleLinkedListTests
+    {
+        [Fact]
+        public void CountWhenEmptyListShouldReturn0()
+        {
+            SingleLinkedList<int> testSLList = new SingleLinkedList<int>();
+
+            Assert.Equal(0, testSLList.Count);
+        }
+    }
+}
