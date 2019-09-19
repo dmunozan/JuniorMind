34ea71734e227260971e929472e3d41643095a17
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-19 13:58:20 +0300

    Added List property and initialization

--- Collections/Node.cs
+++ Collections/Node.cs
@@ -6,11 +6,14 @@
         {
             Value = value;
             NextNode = null;
+            List = null;
         }
 
         public T Value { get; }
 
         public Node<T> NextNode { get; set; }
+
+        public SingleLinkedList<T> List { get; set; }
 
         public override bool Equals(object obj)
         {
