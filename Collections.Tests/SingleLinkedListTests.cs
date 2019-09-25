75cb099a34b26ae59bbed1505faaa70ffc2b5b33
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-25 13:39:41 +0300

    Added test AddBeforeWhenNodeExistShouldAddNewNodeBeforeNode

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -444,5 +444,34 @@ namespace Collections.Tests
             Assert.Equal(node1, testSLList.First);
             Assert.Equal(node1, testSLList.Last);
         }
+
+        [Fact]
+        public void AddBeforeWhenNodeExistShouldAddNewNodeBeforeNode()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>("node1");
+            Node<string> node2 = new Node<string>("node2");
+            Node<string> newNode = new Node<string>("newNode");
+
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+
+            testSLList.AddBefore(node2, newNode);
+
+            Assert.Equal(3, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal(newNode, testSLList.First.NextNode);
+            Assert.Equal("newNode", testSLList.First.NextNode.Value);
+            Assert.Equal(testSLList, newNode.List);
+            Assert.Equal(node2, newNode.NextNode);
+        }
     }
 }
