5c641d79baf7fdb05836db025eff51314896a2be
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-27 12:10:09 +0300

    Added ICollection interface

--- Collections/SingleLinkedList.cs
+++ Collections/SingleLinkedList.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Collections
 {
-    public class SingleLinkedList<T>
+    public class SingleLinkedList<T> : ICollection<T>
     {
         public SingleLinkedList()
         {
@@ -16,6 +18,8 @@ namespace Collections
         public Node<T> First { get; private set; }
 
         public Node<T> Last { get; private set; }
+
+        public bool IsReadOnly => throw new NotImplementedException();
 
         public void AddFirst(Node<T> node)
         {
@@ -150,6 +154,41 @@ namespace Collections
             this.Last = node;
             node.List = this;
             this.Count++;
+        }
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
         }
     }
 }
