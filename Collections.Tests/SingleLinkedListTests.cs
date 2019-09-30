1cfbd63e4d4aa54377a5f4a9126703add6132e88
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-09-30 19:26:46 +0300

    Added already passing test

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1041,5 +1041,27 @@ namespace Collections.Tests
             Assert.Equal(foundNode, node4);
             Assert.Equal(node5, foundNode.NextNode);
         }
+
+        [Fact]
+        public void FindLastWhenTNoExistShouldReturnNull()
+        {
+            SingleLinkedList<int> testSLList = new SingleLinkedList<int>();
+
+            Node<int> node1 = new Node<int>(1);
+            Node<int> node2 = new Node<int>(2);
+            Node<int> node3 = new Node<int>(3);
+            Node<int> node4 = new Node<int>(2);
+            Node<int> node5 = new Node<int>(1);
+
+            testSLList.AddLast(node1);
+            testSLList.AddLast(node2);
+            testSLList.AddLast(node3);
+            testSLList.AddLast(node4);
+            testSLList.AddLast(node5);
+
+            Node<int> foundNode = testSLList.FindLast(6);
+
+            Assert.Null(foundNode);
+        }
     }
 }
