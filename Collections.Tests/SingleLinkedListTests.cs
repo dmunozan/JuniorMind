56e91a19514628c0ea4272658e746aa0cfaa8967
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-01 19:34:35 +0300

    Added test ContainsWhenExistAndNullValueShouldReturnTrue

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1505,5 +1505,22 @@ namespace Collections.Tests
 
             Assert.True(testSLList.Contains("node2"));
         }
+
+        [Fact]
+        public void ContainsWhenExistAndNullValueShouldReturnTrue()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>(null);
+            Node<string> node2 = new Node<string>("node2");
+
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+
+            Assert.True(testSLList.Contains(null));
+        }
     }
 }
