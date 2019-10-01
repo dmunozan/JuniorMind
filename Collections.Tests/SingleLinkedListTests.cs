afaec5a1aa364714202fe97a7e9f098d30ddd4a4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-01 17:58:11 +0300

    Added test RemoveFirstWhenNoEmptyListShouldRemoveFirstNode

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1314,5 +1314,29 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.First.NextNode);
             Assert.Equal(testSLList, testSLList.First.List);
         }
+
+        [Fact]
+        public void RemoveFirstWhenNoEmptyListShouldRemoveFirstNode()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>("node1");
+            Node<string> node2 = new Node<string>("node2");
+
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+
+            testSLList.RemoveFirst();
+
+            Assert.Equal(1, testSLList.Count);
+            Assert.Equal(node2, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+        }
     }
 }
