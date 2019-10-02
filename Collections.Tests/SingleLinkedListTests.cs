de7cbc22749d4f7ba6fd3316ec41e482053c0bec
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-02 20:13:21 +0300

    Added test CopyToWhenDestinationArrayLengthIsSmallerThanCountShouldThrowExceptionAndDoNothing

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1621,5 +1621,25 @@ namespace Collections.Tests
             Assert.Null(destinationArray[1]);
             Assert.Null(destinationArray[2]);
         }
+
+        [Fact]
+        public void CopyToWhenDestinationArrayLengthIsSmallerThanCountShouldThrowExceptionAndDoNothing()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>("node1");
+            Node<string> node2 = new Node<string>("node2");
+            Node<string> node3 = new Node<string>("node3");
+
+            testSLList.AddFirst(node3);
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            string[] destinationArray = new string[2];
+
+            Assert.Throws<ArgumentException>(() => testSLList.CopyTo(destinationArray, 0));
+
+            Assert.Null(destinationArray[0]);
+            Assert.Null(destinationArray[1]);
+        }
     }
 }
