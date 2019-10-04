27c85d701f9d9f3d244e74e51b402e4ce21bdf2c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-04 08:48:13 +0300

    Added already passing test ClearWhenSLLIsReadOnlyShouldThrowException

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1740,6 +1740,35 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void ClearWhenSLLIsReadOnlyShouldThrowException()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>("node1");
+            Node<string> node2 = new Node<string>("node2");
+
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+
+            testSLList.ToReadOnly();
+
+            Assert.Throws<NotSupportedException>(() => testSLList.Clear());
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+        }
+
+        [Fact]
         public void ContainsWhenExistShouldReturnTrue()
         {
             SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
