2d9d29c5a9cbecfb694939fe9543f1dfcf80ede8
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-04 08:44:03 +0300

    Added already passing test RemoveFirstWhenSLLIsReadOnlyShouldThrowException

--- Collections.Tests/SingleLinkedListTests.cs
+++ Collections.Tests/SingleLinkedListTests.cs
@@ -1569,6 +1569,35 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void RemoveFirstWhenSLLIsReadOnlyShouldThrowException()
+        {
+            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
+            Node<string> node1 = new Node<string>("node1");
+            Node<string> node2 = new Node<string>("node2");
+
+            testSLList.AddFirst(node2);
+            testSLList.AddFirst(node1);
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+
+            testSLList.ToReadOnly();
+
+            Assert.Throws<NotSupportedException>(() => testSLList.RemoveFirst());
+
+            Assert.Equal(2, testSLList.Count);
+            Assert.Equal(node1, testSLList.First);
+            Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(testSLList, testSLList.First.List);
+        }
+
+        [Fact]
         public void RemoveLastWhenNoEmptyListShouldRemoveLastNode()
         {
             SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
