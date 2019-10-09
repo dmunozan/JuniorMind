8a3d5f2e224ae6a6c0cf1bb91525de1d95f8405d
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-09 21:58:37 +0300

    Class created as a copy of Node.cs to modify it into a double linked node

---
+++ Collections/DNode.cs
@@ -0,0 +1,30 @@
+﻿namespace Collections
+{
+    public class Node<T>
+    {
+        public Node(T value)
+        {
+            Value = value;
+            NextNode = null;
+            List = null;
+        }
+
+        public T Value { get; }
+
+        public Node<T> NextNode { get; set; }
+
+        public SingleLinkedList<T> List { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+            {
+                return false;
+            }
+
+            Node<T> objNode = (Node<T>)obj;
+
+            return this.Value.Equals(objNode.Value);
+        }
+    }
+}
