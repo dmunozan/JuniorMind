745adbc812539e4476d18ecad9af3c413094963a
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-09 22:00:10 +0300

    Class created Created as a copy of NodeTests to modify it with the tests for the double linked node

---
+++ Collections.Tests/DNodeTests.cs
@@ -0,0 +1,79 @@
+﻿using Xunit;
+
+namespace Collections.Tests
+{
+    public class NodeTests
+    {
+        [Fact]
+        public void ValueWhenAnyShouldReturnValueSavedOnNode()
+        {
+            Node<int> testNode = new Node<int>(0);
+
+            Assert.Equal(0, testNode.Value);
+        }
+
+        [Fact]
+        public void NextNodeWhenNoNodeShouldReturnNull()
+        {
+            Node<int> testNode = new Node<int>(0);
+
+            Assert.Null(testNode.NextNode);
+        }
+
+        [Fact]
+        public void NextNodeWhenNodeShouldReturnAssignedNode()
+        {
+            Node<int> testNode = new Node<int>(0);
+            Node<int> testNextNode = new Node<int>(1);
+
+            testNode.NextNode = testNextNode;
+
+            Assert.Equal(testNextNode, testNode.NextNode);
+        }
+
+        [Fact]
+        public void ListWhenNoAssignedShouldReturnNull()
+        {
+            Node<int> testNode = new Node<int>(0);
+
+            Assert.Null(testNode.List);
+        }
+
+        [Fact]
+        public void ListWhenAssignedShouldReturnSingleLinkedList()
+        {
+            SingleLinkedList<int> testSLList = new SingleLinkedList<int>();
+            Node<int> testNode = new Node<int>(0);
+
+            testNode.List = testSLList;
+
+            Assert.Equal(testSLList, testNode.List);
+        }
+
+        [Fact]
+        public void EqualsWhenSameValueShouldReturnTrue()
+        {
+            Node<int> testNode = new Node<int>(0);
+            Node<int> sameNode = new Node<int>(0);
+
+            Assert.True(testNode.Equals(sameNode));
+        }
+
+        [Fact]
+        public void EqualsWhenNullShouldReturnFalse()
+        {
+            Node<int> testNode = new Node<int>(0);
+
+            Assert.False(testNode.Equals(null));
+        }
+
+        [Fact]
+        public void EqualsWhenDifferentValueShouldReturnFalse()
+        {
+            Node<int> testNode = new Node<int>(0);
+            Node<int> notSameNode = new Node<int>(1);
+
+            Assert.False(testNode.Equals(notSameNode));
+        }
+    }
+}
