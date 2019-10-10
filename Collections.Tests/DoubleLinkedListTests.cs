943bcd92489fd71df22d124ef06063ff0c9c4a24
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-10 22:16:39 +0300

    Modified test FindWhenTNoExistShouldReturnNull Replaced instances of Node with DNode and SingleLinkedList with DoubleLinkedList Test passed without the need to modify the code

--- Collections.Tests/DoubleLinkedListTests.cs
+++ Collections.Tests/DoubleLinkedListTests.cs
@@ -1153,13 +1153,13 @@ namespace Collections.Tests
         [Fact]
         public void FindWhenTNoExistShouldReturnNull()
         {
-            SingleLinkedList<int> testSLList = new SingleLinkedList<int>();
+            DoubleLinkedList<int> testSLList = new DoubleLinkedList<int>();
 
-            Node<int> node1 = new Node<int>(1);
-            Node<int> node2 = new Node<int>(2);
-            Node<int> node3 = new Node<int>(3);
-            Node<int> node4 = new Node<int>(2);
-            Node<int> node5 = new Node<int>(1);
+            DNode<int> node1 = new DNode<int>(1);
+            DNode<int> node2 = new DNode<int>(2);
+            DNode<int> node3 = new DNode<int>(3);
+            DNode<int> node4 = new DNode<int>(2);
+            DNode<int> node5 = new DNode<int>(1);
 
             testSLList.AddLast(node1);
             testSLList.AddLast(node2);
@@ -1167,7 +1167,7 @@ namespace Collections.Tests
             testSLList.AddLast(node4);
             testSLList.AddLast(node5);
 
-            Node<int> foundNode = testSLList.Find(6);
+            DNode<int> foundNode = testSLList.Find(6);
 
             Assert.Null(foundNode);
         }
