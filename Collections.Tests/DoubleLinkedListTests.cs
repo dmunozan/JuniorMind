469da9ab99704d5248b1bad572b400eb49e85c57
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-10 19:04:05 +0300

    Modified test AddAfterWhenNodeExistShouldAddNewNodeAfterNode Replaced instances of Node with DNode and SingleLinkedList with DoubleLinkedList Added assert

--- Collections.Tests/DoubleLinkedListTests.cs
+++ Collections.Tests/DoubleLinkedListTests.cs
@@ -238,10 +238,10 @@ namespace Collections.Tests
         [Fact]
         public void AddAfterWhenNodeExistShouldAddNewNodeAfterNode()
         {
-            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
-            Node<string> node1 = new Node<string>("node1");
-            Node<string> node2 = new Node<string>("node2");
-            Node<string> newNode = new Node<string>("newNode");
+            DoubleLinkedList<string> testSLList = new DoubleLinkedList<string>();
+            DNode<string> node1 = new DNode<string>("node1");
+            DNode<string> node2 = new DNode<string>("node2");
+            DNode<string> newNode = new DNode<string>("newNode");
 
             testSLList.AddFirst(node2);
             testSLList.AddFirst(node1);
@@ -251,6 +251,7 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.Last);
             Assert.Equal("node1", testSLList.First.Value);
             Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node1, testSLList.Last.PreviousNode);
             Assert.Equal(testSLList, testSLList.First.List);
 
             testSLList.AddAfter(node1, newNode);
@@ -260,8 +261,10 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.Last);
             Assert.Equal(newNode, testSLList.First.NextNode);
             Assert.Equal("newNode", testSLList.First.NextNode.Value);
+            Assert.Equal(newNode, testSLList.Last.PreviousNode);
             Assert.Equal(testSLList, newNode.List);
             Assert.Equal(node2, newNode.NextNode);
+            Assert.Equal(node1, newNode.PreviousNode);
         }
 
         [Fact]
