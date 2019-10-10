6c2cd5ca1fe282785440907bd0bf413413ae158b
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-10 19:55:18 +0300

    Modified test AddAfterWhenNodeIsLastNodeShouldAddTAsLastNode Replaced instances of Node with DNode and SingleLinkedList with DoubleLinkedList Added assert Test passed without the need to modify the code

--- Collections.Tests/DoubleLinkedListTests.cs
+++ Collections.Tests/DoubleLinkedListTests.cs
@@ -426,9 +426,9 @@ namespace Collections.Tests
         [Fact]
         public void AddAfterWhenNodeIsLastNodeShouldAddTAsLastNode()
         {
-            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
-            Node<string> node1 = new Node<string>("node1");
-            Node<string> node2 = new Node<string>("node2");
+            DoubleLinkedList<string> testSLList = new DoubleLinkedList<string>();
+            DNode<string> node1 = new DNode<string>("node1");
+            DNode<string> node2 = new DNode<string>("node2");
 
             testSLList.AddFirst(node2);
             testSLList.AddFirst(node1);
@@ -438,17 +438,20 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.Last);
             Assert.Equal("node1", testSLList.First.Value);
             Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node1, testSLList.Last.PreviousNode);
             Assert.Equal(testSLList, testSLList.First.List);
 
-            Node<string> newNode = testSLList.AddAfter(node2, "newNode");
+            DNode<string> newNode = testSLList.AddAfter(node2, "newNode");
 
             Assert.Equal(3, testSLList.Count);
             Assert.Equal(node1, testSLList.First);
             Assert.Equal(newNode, testSLList.Last);
             Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node2, testSLList.Last.PreviousNode);
             Assert.Equal("newNode", testSLList.Last.Value);
             Assert.Equal(testSLList, newNode.List);
             Assert.Null(newNode.NextNode);
+            Assert.Equal(node2, newNode.PreviousNode);
         }
 
         [Fact]
