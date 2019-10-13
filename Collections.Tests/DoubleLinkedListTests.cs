9aa2e0abe3addd5d5108202dc0ea5dfff4fe7f5f
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-13 11:34:25 +0300

    Modified test RemoveFirstWhenSLLIsReadOnlyShouldThrowException Replaced instances of Node with DNode and SingleLinkedList with DoubleLinkedList Added assert Test passed without the need to modify the code

--- Collections.Tests/DoubleLinkedListTests.cs
+++ Collections.Tests/DoubleLinkedListTests.cs
@@ -1663,9 +1663,9 @@ namespace Collections.Tests
         [Fact]
         public void RemoveFirstWhenSLLIsReadOnlyShouldThrowException()
         {
-            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
-            Node<string> node1 = new Node<string>("node1");
-            Node<string> node2 = new Node<string>("node2");
+            DoubleLinkedList<string> testSLList = new DoubleLinkedList<string>();
+            DNode<string> node1 = new DNode<string>("node1");
+            DNode<string> node2 = new DNode<string>("node2");
 
             testSLList.AddFirst(node2);
             testSLList.AddFirst(node1);
@@ -1675,6 +1675,7 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.Last);
             Assert.Equal("node1", testSLList.First.Value);
             Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node1, testSLList.Last.PreviousNode);
             Assert.Equal(testSLList, testSLList.First.List);
 
             testSLList.ToReadOnly();
@@ -1686,6 +1687,7 @@ namespace Collections.Tests
             Assert.Equal(node2, testSLList.Last);
             Assert.Equal("node1", testSLList.First.Value);
             Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node1, testSLList.Last.PreviousNode);
             Assert.Equal(testSLList, testSLList.First.List);
         }
 
