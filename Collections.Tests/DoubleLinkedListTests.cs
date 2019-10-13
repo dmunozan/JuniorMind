c2cccaa8a93d6ca4185a250dfd3eb58ed5386299
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-13 18:41:24 +0300

    Modified test ContainsWhenExistShouldReturnTrue Replaced instances of Node with DNode and SingleLinkedList with DoubleLinkedList Added assert Test passed without the need to modify the code

--- Collections.Tests/DoubleLinkedListTests.cs
+++ Collections.Tests/DoubleLinkedListTests.cs
@@ -1885,9 +1885,9 @@ namespace Collections.Tests
         [Fact]
         public void ContainsWhenExistShouldReturnTrue()
         {
-            SingleLinkedList<string> testSLList = new SingleLinkedList<string>();
-            Node<string> node1 = new Node<string>("node1");
-            Node<string> node2 = new Node<string>("node2");
+            DoubleLinkedList<string> testSLList = new DoubleLinkedList<string>();
+            DNode<string> node1 = new DNode<string>("node1");
+            DNode<string> node2 = new DNode<string>("node2");
 
             testSLList.AddFirst(node2);
             testSLList.AddFirst(node1);
@@ -1895,6 +1895,10 @@ namespace Collections.Tests
             Assert.Equal(2, testSLList.Count);
             Assert.Equal(node1, testSLList.First);
             Assert.Equal(node2, testSLList.Last);
+            Assert.Equal("node1", testSLList.First.Value);
+            Assert.Equal(node2, testSLList.First.NextNode);
+            Assert.Equal(node1, testSLList.Last.PreviousNode);
+            Assert.Equal(testSLList, testSLList.First.List);
 
             Assert.True(testSLList.Contains("node2"));
         }
