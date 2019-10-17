fdb24feaa0cd62c0baefdcaeb1638f7102f77644
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-17 19:00:12 +0300

    Added test CountWhenDictionaryIsEmptyShouldReturn0

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -4,5 +4,12 @@ namespace Collections.Tests
 {
     public class DictionaryTests
     {
+        [Fact]
+        public void CountWhenDictionaryIsEmptyShouldReturn0()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>();
+
+            Assert.Equal(0, testDict.Count);
+        }
     }
 }
