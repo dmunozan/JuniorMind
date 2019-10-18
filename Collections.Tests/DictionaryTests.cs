c2fbd2646b510d8d6535cadfe2274b0e420d8ae8
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-18 19:27:09 +0300

    Modified class Moved Add test after properties tests

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -27,16 +27,6 @@ namespace Collections.Tests
         }
 
         [Fact]
-        public void AddWhenDictionaryIsEmptyAndThereIsAvailableSpaceShouldAddKeyValue()
-        {
-            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
-
-            testDict.Add(1, "a");
-
-            Assert.Equal(1, testDict.Count);
-        }
-
-        [Fact]
         public void KeysWhenDictionaryIsEmptyShouldReturnEmptyCollection()
         {
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
@@ -108,6 +98,16 @@ namespace Collections.Tests
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
 
             Assert.False(testDict.IsReadOnly);
+        }
+
+        [Fact]
+        public void AddWhenDictionaryIsEmptyAndThereIsAvailableSpaceShouldAddKeyValue()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+
+            Assert.Equal(1, testDict.Count);
         }
 
         [Fact]
