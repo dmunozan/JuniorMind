26f5a9484be24961323192b9fa8eef6d2a35248a
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-19 20:24:08 +0300

    Added test IndexerWhenSetAndKeyNoExistShouldAddNewPairKeyValue

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -124,6 +124,20 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void IndexerWhenSetAndKeyNoExistShouldAddNewPairKeyValue()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+            testDict.Add(2, "b");
+            testDict.Add(3, "c");
+
+            testDict[4] = "d";
+
+            Assert.Equal("d", testDict[4]);
+        }
+
+        [Fact]
         public void IsReadOnlyWhenNewDictionaryShouldReturnFalse()
         {
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
