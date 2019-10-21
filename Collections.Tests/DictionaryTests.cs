246d0884e93e7e7f5c0065de5c9cb28500e70d01
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-21 19:38:36 +0300

    Added test ContainsKeyWhenExistShouldReturnTrue

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -273,6 +273,18 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void ContainsKeyWhenExistShouldReturnTrue()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+            testDict.Add(2, "b");
+            testDict.Add(3, "c");
+
+            Assert.True(testDict.ContainsKey(1));
+        }
+
+        [Fact]
         public void ToReadOnlyWhenAnyShouldSetIsReadOnlyAsTrue()
         {
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
