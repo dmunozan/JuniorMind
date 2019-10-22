1521fc3c8ca6641db9de8cafe4506bacbe45e5eb
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-22 13:44:35 +0300

    Added test CopyToWhenIndexIsNegativeShouldThrowException

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -409,6 +409,20 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void CopyToWhenIndexIsNegativeShouldThrowException()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+            testDict.Add(2, "b");
+            testDict.Add(3, "c");
+
+            KeyValuePair<int, string>[] destinationArray = new KeyValuePair<int, string>[4];
+
+            Assert.Throws<ArgumentOutOfRangeException>(() => testDict.CopyTo(destinationArray, -1));
+        }
+
+        [Fact]
         public void ToReadOnlyWhenAnyShouldSetIsReadOnlyAsTrue()
         {
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
