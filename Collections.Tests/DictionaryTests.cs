cf6e56aba681440c16c44b4bca25a4c8dfd498a1
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-23 12:53:41 +0300

    Added test TryGetValueWhenKeyExistShouldReturnTrueAndValue

--- Collections.Tests/DictionaryTests.cs
+++ Collections.Tests/DictionaryTests.cs
@@ -589,6 +589,21 @@ namespace Collections.Tests
         }
 
         [Fact]
+        public void TryGetValueWhenKeyExistShouldReturnTrueAndValue()
+        {
+            Dictionary<int, string> testDict = new Dictionary<int, string>(5);
+
+            testDict.Add(1, "a");
+            testDict.Add(2, "b");
+            testDict.Add(3, "c");
+
+            string value;
+            
+            Assert.True(testDict.TryGetValue(2, out value));
+            Assert.Equal("b", value);
+        }
+
+        [Fact]
         public void ToReadOnlyWhenAnyShouldSetIsReadOnlyAsTrue()
         {
             Dictionary<int, string> testDict = new Dictionary<int, string>(5);
