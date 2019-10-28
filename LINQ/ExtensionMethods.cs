fda6b7ca0d3606b6446ca7538cbd9be2b3c162b0
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-28 20:04:22 +0200

    Created All<TSource> method

--- LINQ/ExtensionMethods.cs
+++ LINQ/ExtensionMethods.cs
@@ -1,6 +1,26 @@
-﻿namespace LINQ
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQ
 {
     public static class ExtensionMethods
     {
+        public static bool All<TSource>(this IEnumerable<TSource> source, Func<TSource, bool> predicate)
+        {
+            if (source == null)
+            {
+                return false;
+            }
+
+            foreach (var element in source)
+            {
+                if (!predicate(element))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
     }
 }
