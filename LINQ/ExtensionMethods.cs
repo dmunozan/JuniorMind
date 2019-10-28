b4e4afec23e456a814b5f15350fbcc5a27a52725
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-10-28 20:12:52 +0200

    Modified All<TSource> replaced return with throw exception

--- LINQ/ExtensionMethods.cs
+++ LINQ/ExtensionMethods.cs
@@ -9,7 +9,7 @@ namespace LINQ
         {
             if (source == null)
             {
-                return false;
+                throw new ArgumentNullException(nameof(source), "Source argument is null");
             }
 
             foreach (var element in source)
