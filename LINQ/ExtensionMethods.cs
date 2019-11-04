e8dfa0274b54fff5120f75a4cc006435d6bf1923
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-04 21:43:22 +0200

    Created Intersect

--- LINQ/ExtensionMethods.cs
+++ LINQ/ExtensionMethods.cs
@@ -242,6 +242,16 @@ namespace LINQ
             return result.Distinct(comparer);
         }
 
+        public static IEnumerable<TSource> Intersect<TSource>(
+            this IEnumerable<TSource> first,
+            IEnumerable<TSource> second,
+            IEqualityComparer<TSource> comparer)
+        {
+            Console.WriteLine(first + "" + second + comparer);
+
+            return new List<TSource>();
+        }
+
         private static IEnumerable<TResult> InternalSelect<TSource, TResult>(this IEnumerable<TSource> source, Func<TSource, TResult> selector)
         {
             foreach (var element in source)
