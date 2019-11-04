4dcb95308165020f0a6fce084b3932d4ff58dc2a
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-04 22:06:07 +0200

    Modified Intersect Replaced return with check for null first

--- LINQ/ExtensionMethods.cs
+++ LINQ/ExtensionMethods.cs
@@ -247,11 +247,13 @@ namespace LINQ
             IEnumerable<TSource> second,
             IEqualityComparer<TSource> comparer)
         {
+            CheckNullElement(first);
+
             List<TSource> result = new List<TSource>();
 
             comparer = comparer ?? EqualityComparer<TSource>.Default;
 
-            if (first == null || second == null)
+            if (second == null)
             {
                 return result;
             }
