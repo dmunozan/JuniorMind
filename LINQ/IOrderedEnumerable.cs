df0e0723da789e28d710648d709abc0092d8d3ed
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-17 10:31:22 +0200

    Modified class Added orderedList to contain the ordered elements Added constructor to add the elements to orderedList Added implementation to GetEnumerator() and IEnumerable.GetEnumerator() to be able to run the tests Created CreateOrderedEnumerable<TKey> and added basic implementation

--- LINQ/IOrderedEnumerable.cs
+++ LINQ/IOrderedEnumerable.cs
@@ -1,18 +1,36 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 
 namespace LINQ
 {
     public class IOrderedEnumerable<TElement> : IEnumerable<TElement>
     {
+        readonly List<TElement> orderedList = new List<TElement>();
+
+        public IOrderedEnumerable(IEnumerable<TElement> source)
+        {
+            orderedList.AddRange(source);
+        }
+
+        public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer)
+        {
+            Console.WriteLine(keySelector + "" + comparer);
+
+            return this;
+        }
+
         public IEnumerator<TElement> GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            foreach (var item in orderedList)
+            {
+                yield return item;
+            }
         }
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new System.NotImplementedException();
+            return this.GetEnumerator();
         }
     }
 }
