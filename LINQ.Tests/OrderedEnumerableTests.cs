8bfec1d1cbff80423e0660bd85f7f49379f5d1e9
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-17 19:02:18 +0200

    Renamed file and edited code to maintain the functionality

---
+++ LINQ.Tests/OrderedEnumerableTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace LINQ.Tests
+{
+    public class OrderedEnumerableTests
+    {
+        [Fact]
+        public void CreateOrderedEnumerableWhenNoElementsShouldReturnEmptySequence()
+        {
+            string[] testArray = {  };
+
+            OrderedEnumerable<string> initialList = new OrderedEnumerable<string>(testArray);
+
+            IOrderedEnumerable<string> orderedList = initialList.CreateOrderedEnumerable(x => 1, Comparer<int>.Default, false);
+
+            Assert.Empty(orderedList);
+        }
+    }
+}
