75307fbbb6b441e9e3c5cdd985e7a90728e2b1a2
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-19 12:18:07 +0200

    Created Range method Added deferred execution implementation

--- LINQ/OtherExtensionMethods.cs
+++ LINQ/OtherExtensionMethods.cs
@@ -1,6 +1,18 @@
-﻿namespace LINQ
+﻿using System.Collections.Generic;
+
+namespace LINQ
 {
     public class OtherExtensionMethods
     {
+        public static IEnumerable<int> Range(int start, int count)
+        {
+            int end = start + count;
+
+            while (start < end)
+            {
+                yield return start;
+                start++;
+            }
+        }
     }
 }
