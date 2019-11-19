91412fcebb3047a418da8c89c8002534f8b05958
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-19 13:00:12 +0200

    Modified Range Added check for count < 0 Moved logic to InternalRange to throw exceptions while keeping deferred execution on the logic

--- LINQ/OtherExtensionMethods.cs
+++ LINQ/OtherExtensionMethods.cs
@@ -1,10 +1,21 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace LINQ
 {
     public class OtherExtensionMethods
     {
         public static IEnumerable<int> Range(int start, int count)
+        {
+            if (count < 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(count), "count is less than 0");
+            }
+
+            return InternalRange(start, count);
+        }
+
+        private static IEnumerable<int> InternalRange(int start, int count)
         {
             int end = start + count;
 
