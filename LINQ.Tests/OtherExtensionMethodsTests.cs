871705a278a983efd79922520ba9b31896760739
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-19 12:17:36 +0200

    Added test RangeWhenValidNumbersShouldReturnIntSequence

--- LINQ.Tests/OtherExtensionMethodsTests.cs
+++ LINQ.Tests/OtherExtensionMethodsTests.cs
@@ -1,8 +1,21 @@
-﻿using Xunit;
+﻿using System.Collections.Generic;
+using Xunit;
 
 namespace LINQ.Tests
 {
     public class OtherExtensionMethodsTests
     {
+        [Fact]
+        public void RangeWhenValidNumbersShouldReturnIntSequence()
+        {
+            IEnumerable<int> numbers = OtherExtensionMethods.Range(1, 5);
+
+            Assert.Collection(numbers,
+                item => Assert.Equal(1, item),
+                item => Assert.Equal(2, item),
+                item => Assert.Equal(3, item),
+                item => Assert.Equal(4, item),
+                item => Assert.Equal(5, item));
+        }
     }
 }
