b994cdf5f13ae2076442adb1edf2b459e6d688df
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-11-27 18:06:13 +0200

    Modified class Completed constructor Added members and TPrimaryKey Created MergeSort and Merge methods for the sorting Renamed orderedList member to orderedEnumerableList and TKey on the method to TSecondaryKey

--- LINQ/OrderedEnumerable.cs
+++ LINQ/OrderedEnumerable.cs
@@ -5,16 +5,29 @@ using System.Linq;
 
 namespace LINQ
 {
-    public class OrderedEnumerable<TElement> : IOrderedEnumerable<TElement>
+    public class OrderedEnumerable<TElement, TPrimaryKey> : IOrderedEnumerable<TElement>
     {
-        readonly List<TElement> orderedList;
+        readonly List<TElement> orderedEnumerableList;
+        readonly Func<TElement, TPrimaryKey> keySelector;
+        readonly IComparer<TPrimaryKey> comparer;
 
-        public OrderedEnumerable(IEnumerable<TElement> source)
+        public OrderedEnumerable(
+            IEnumerable<TElement> source,
+            Func<TElement, TPrimaryKey> keySelector,
+            IComparer<TPrimaryKey> comparer)
         {
-            orderedList = new List<TElement>(source);
+            source ??= new List<TElement>();
+
+            this.keySelector = keySelector;
+            this.comparer = comparer;
+
+            this.orderedEnumerableList = MergeSort(source);
         }
 
-        public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TKey>(Func<TElement, TKey> keySelector, IComparer<TKey> comparer, bool descending)
+        public IOrderedEnumerable<TElement> CreateOrderedEnumerable<TSecondaryKey>(
+            Func<TElement, TSecondaryKey> keySelector,
+            IComparer<TSecondaryKey> comparer,
+            bool descending)
         {
             Console.WriteLine(keySelector + "" + comparer + descending);
 
@@ -23,7 +36,7 @@ namespace LINQ
 
         public IEnumerator<TElement> GetEnumerator()
         {
-            foreach (var item in orderedList)
+            foreach (var item in orderedEnumerableList)
             {
                 yield return item;
             }
@@ -32,6 +45,77 @@ namespace LINQ
         IEnumerator IEnumerable.GetEnumerator()
         {
             return this.GetEnumerator();
+        }
+
+        public List<TElement> MergeSort(
+            IEnumerable<TElement> source)
+        {
+            List<TElement> unorderedList = new List<TElement>(source);
+
+            if (unorderedList.Count <= 1)
+            {
+                return unorderedList;
+            }
+
+            List<TElement> left = new List<TElement>();
+            List<TElement> right = new List<TElement>();
+
+            int middle = unorderedList.Count / 2;
+            for (int i = 0; i < middle; i++)
+            {
+                left.Add(unorderedList[i]);
+            }
+
+            for (int i = middle; i < unorderedList.Count; i++)
+            {
+                right.Add(unorderedList[i]);
+            }
+
+            left = MergeSort(left);
+            right = MergeSort(right);
+
+            return Merge(left, right);
+        }
+
+        public List<TElement> Merge(
+            List<TElement> left,
+            List<TElement> right)
+        {
+            List<TElement> leftList = left ?? new List<TElement>();
+            List<TElement> rightList = right ?? new List<TElement>();
+
+            List<TElement> orderedList = new List<TElement>();
+
+            while (leftList.Count > 0 || rightList.Count > 0)
+            {
+                if (leftList.Count > 0 && rightList.Count > 0)
+                {
+                    if (this.comparer.Compare(
+                        this.keySelector(leftList.First()),
+                        this.keySelector(rightList.First())) <= 0)
+                    {
+                        orderedList.Add(leftList.First());
+                        leftList.Remove(leftList.First());
+                    }
+                    else
+                    {
+                        orderedList.Add(rightList.First());
+                        rightList.Remove(rightList.First());
+                    }
+                }
+                else if (leftList.Count > 0)
+                {
+                    orderedList.Add(leftList.First());
+                    leftList.Remove(leftList.First());
+                }
+                else if (rightList.Count > 0)
+                {
+                    orderedList.Add(rightList.First());
+                    rightList.Remove(rightList.First());
+                }
+            }
+
+            return orderedList;
         }
     }
 }
