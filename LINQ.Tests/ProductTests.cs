3739a2a89a8165367879a61e67f131480a41543c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-11 19:13:44 +0200

    Added already passing test NameWhenSetShouldSetProductName

--- LINQ.Tests/ProductTests.cs
+++ LINQ.Tests/ProductTests.cs
@@ -11,5 +11,23 @@ namespace LINQ.Tests
 
             Assert.Equal("apricot", testProduct.Name);
         }
+
+        [Fact]
+        public void NameWhenSetShouldSetProductName()
+        {
+            Product testProduct = new Product("apricot", 8);
+
+            testProduct.Name = "orange";
+
+            Assert.Equal("orange", testProduct.Name);
+        }
+
+        [Fact]
+        public void QuantityWhenGetShouldReturnProductQuantity()
+        {
+            Product testProduct = new Product("apricot", 8);
+
+            Assert.Equal(8, testProduct.Quantity);
+        }
     }
 }
