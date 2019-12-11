285a482e21404e43a1599b4e6ebc5870354e6c90
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-11 19:08:21 +0200

    Created class Added constructor and Name propertie

---
+++ LINQ/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace LINQ
+{
+    public class Product
+    {
+        readonly int quantity;
+
+        public Product(string name, int quantity)
+        {
+            Name = name;
+            this.quantity = quantity;
+        }
+
+        public string Name { get; set; }
+    }
+}
