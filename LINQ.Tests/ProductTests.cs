93f52ac0eb2d0ea2c78e4a25f9abfd33d3e24d38
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-11 19:07:51 +0200

    Created class and added test NameWhenGetShouldReturnProductName

---
+++ LINQ.Tests/ProductTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace LINQ.Tests
+{
+    public class ProductTests
+    {
+        [Fact]
+        public void NameWhenGetShouldReturnProductName()
+        {
+            Product testProduct = new Product("apricot", 8);
+
+            Assert.Equal("apricot", testProduct.Name);
+        }
+    }
+}
