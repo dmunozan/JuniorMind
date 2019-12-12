658df193be2068bea232bd2617d03c8a6b2eefff
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-12 19:27:19 +0200

    Class created Added constructor, GetEnumerator and productList field

---
+++ LINQ/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace LINQ
+{
+    public class Stock : IEnumerable
+    {
+        readonly Dictionary<string, Product> productList;
+
+        public Stock()
+        {
+            productList = new Dictionary<string, Product>();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return productList.GetEnumerator();
+        }
+    }
+}
