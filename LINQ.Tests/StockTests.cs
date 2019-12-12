2d59c06549592c6d865cf0b47f81158c14ff09da
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-12 19:25:18 +0200

    Test added StockWhenAnyShouldCreateEmptyStock

--- LINQ.Tests/StockTests.cs
+++ LINQ.Tests/StockTests.cs
@@ -4,5 +4,12 @@ namespace LINQ.Tests
 {
     public class StockTests
     {
+        [Fact]
+        public void StockWhenAnyShouldCreateEmptyStock()
+        {
+            Stock test = new Stock();
+
+            Assert.Empty(test);
+        }
     }
 }
