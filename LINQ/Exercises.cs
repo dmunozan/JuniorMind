bf840c688eab82c3df2c088fb4a5b9467d3eaf71
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-30 16:36:21 +0200

    Created class and NumberOfVocals method

---
+++ LINQ/Exercises.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LINQ
+{
+    public class Exercises
+    {
+        public int NumberOfVocals(string text)
+        {
+            Console.WriteLine(text);
+
+            return 0;
+        }
+    }
+}
