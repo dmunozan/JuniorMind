3592559d98f56e7d1308a47537f6e9d571805f4c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-30 16:35:55 +0200

    Created class and added test NumberOfVocalsWhenEmptyStringShouldReturn0

---
+++ LINQ.Tests/ExercisesTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace LINQ.Tests
+{
+    public class ExercisesTests
+    {
+        [Fact]
+        public void NumberOfVocalsWhenEmptyStringShouldReturn0()
+        {
+            Exercises testExercise = new Exercises();
+            
+            Assert.Equal(0, testExercise.NumberOfVocals(""));
+        }
+    }
+}
