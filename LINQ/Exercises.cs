266049836f70de0beac4607da4b03eccc6a73d43
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2019-12-30 16:41:26 +0200

    Modified NumberOfVocals Added Count when char is vocal

--- LINQ/Exercises.cs
+++ LINQ/Exercises.cs
@@ -6,9 +6,15 @@ namespace LINQ
     {
         public int NumberOfVocals(string text)
         {
-            Console.WriteLine(text);
-
-            return 0;
+            return text.Count(
+                c =>
+                {
+                    var isA = c == 'a' || c == 'A';
+                    var isE = isA || c == 'e' || c == 'E';
+                    var isI = isE || c == 'i' || c == 'I';
+                    var isO = isI || c == 'o' || c == 'O';
+                    return isO || c == 'u' || c == 'U';
+                });
         }
     }
 }
