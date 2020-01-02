a5af4553c421bccf18ce803f0d54f7d4777a6bb4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-02 17:36:57 +0200

    Removed several tests as the functionality has been moved to NumberOfLetters

--- LINQ.Tests/ExercisesTests.cs
+++ LINQ.Tests/ExercisesTests.cs
@@ -5,54 +5,6 @@ namespace LINQ.Tests
     public class ExercisesTests
     {
         [Fact]
-        public void NumberOfVowelsWhenEmptyStringShouldReturn0()
-        {
-            Exercises testExercise = new Exercises();
-            
-            Assert.Equal(0, testExercise.NumberOfVowels(""));
-        }
-
-        [Fact]
-        public void NumberOfVowelsWhenNoEmptyStringShouldReturnNumberOfVowels()
-        {
-            Exercises testExercise = new Exercises();
-
-            Assert.Equal(5, testExercise.NumberOfVowels("This is a test string"));
-        }
-
-        [Fact]
-        public void NumberOfVowelsWhenNullStringShouldReturn0()
-        {
-            Exercises testExercise = new Exercises();
-
-            Assert.Equal(0, testExercise.NumberOfVowels(null));
-        }
-
-        [Fact]
-        public void NumberOfConsonantsWhenEmptyStringShouldReturn0()
-        {
-            Exercises testExercise = new Exercises();
-
-            Assert.Equal(0, testExercise.NumberOfConsonants(""));
-        }
-
-        [Fact]
-        public void NumberOfConsonantsWhenNoEmptyStringShouldReturnNumberOfConsonants()
-        {
-            Exercises testExercise = new Exercises();
-
-            Assert.Equal(12, testExercise.NumberOfConsonants("This is a test string."));
-        }
-
-        [Fact]
-        public void NumberOfConsonantsWhenNullStringShouldReturn0()
-        {
-            Exercises testExercise = new Exercises();
-
-            Assert.Equal(0, testExercise.NumberOfConsonants(null));
-        }
-
-        [Fact]
         public void NumberOfLettersWhenEmptyStringShouldReturn0ConsonantsAnd0Vowels()
         {
             Exercises testExercise = new Exercises();
