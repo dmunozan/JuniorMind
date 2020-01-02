3b9a47317f7d6a9932b003e63c0c4ee384385197
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-02 17:29:49 +0200

    Modified NumberOfLetters - Added letterCount - Saved values as pairs of key and value - Added loop to update letterCount if needed

--- LINQ/Exercises.cs
+++ LINQ/Exercises.cs
@@ -27,18 +27,32 @@ namespace LINQ
 
         public (int consonants, int vowels) NumberOfLetters(string text)
         {
+            var letterCount = (consonants: 0, vowels: 0);
+
             if (string.IsNullOrEmpty(text))
             {
-                return (0, 0);
+                return letterCount;
             }
 
             const string vowels = "aeiou";
 
-            int[] numOfLetters =
+            var groupsOfLetters =
                 string.Concat(text.ToLowerInvariant().Split(".?! ;:,".ToCharArray(), StringSplitOptions.RemoveEmptyEntries)).
-                GroupBy(c => vowels.Contains(c), (key, chars) => chars.Count()).ToArray();
+                GroupBy(c => vowels.Contains(c), (key, chars) => new { Key = key, Count = chars.Count() });
 
-            return (numOfLetters[0], numOfLetters[1]);
+            foreach (var group in groupsOfLetters)
+            {
+                if (group.Key)
+                {
+                    letterCount.vowels = group.Count;
+                }
+                else
+                {
+                    letterCount.consonants = group.Count;
+                }
+            }
+
+            return letterCount;
         }
     }
 }
