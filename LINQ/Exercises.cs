d89bdd50aa2e4f99d61f45810df1aecbc1e00558
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-02 17:37:29 +0200

    Removed methods as the functionality has been moved to NumberOfLetters

--- LINQ/Exercises.cs
+++ LINQ/Exercises.cs
@@ -5,26 +5,6 @@ namespace LINQ
 {
     public class Exercises
     {
-        public int NumberOfVowels(string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return 0;
-            }
-
-            return text.Split("aeiou".ToCharArray()).Length - 1;
-        }
-
-        public int NumberOfConsonants(string text)
-        {
-            if (string.IsNullOrEmpty(text))
-            {
-                return 0;
-            }
-
-            return text.Split(" .?!".ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Select(w => w.Length).Sum() - NumberOfVowels(text);
-        }
-
         public (int consonants, int vowels) NumberOfLetters(string text)
         {
             var letterCount = (consonants: 0, vowels: 0);
