fd37f386b414dba72fcd7f588c2e7771b2ec06c5
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-11 16:41:22 +0200

    Created class - Added test IdWhenGetShouldReturnFeatureId

---
+++ LINQ.Tests/FeatureTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace LINQ.Tests
+{
+    public class FeatureTests
+    {
+        [Fact]
+        public void IdWhenGetShouldReturnFeatureId()
+        {
+            Feature testFeature = new Feature(1);
+
+            Assert.Equal(1, testFeature.Id);
+        }
+    }
+}
