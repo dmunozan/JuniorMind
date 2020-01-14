10b3a0b38ea42cf17c5874ee7916adc47e486e50
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-14 17:58:09 +0200

    Created class - Added constructor - Added Id property

---
+++ LINQ/TestResults.cs
@@ -0,0 +1,12 @@
+﻿namespace LINQ
+{
+    public class TestResults
+    {
+        public TestResults(string id)
+        {
+            Id = id;
+        }
+
+        public string Id { get; }
+    }
+}
