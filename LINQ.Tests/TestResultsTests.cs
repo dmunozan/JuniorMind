c4bf6a1721668a1901ca437d1e93442cdd50ea18
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-14 17:57:41 +0200

    Created class - Added test IdWhenGetShouldReturnId

---
+++ LINQ.Tests/TestResultsTests.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+
+namespace LINQ.Tests
+{
+    public class TestResultsTests
+    {
+        [Fact]
+        public void IdWhenGetShouldReturnId()
+        {
+            TestResults result = new TestResults("A");
+
+            Assert.Equal("A", result.Id);
+        }
+    }
+}
