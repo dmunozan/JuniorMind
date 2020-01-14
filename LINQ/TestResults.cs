2acae8d2a8d6d6b1fe76133963e4cf796e24a808
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-14 18:04:13 +0200

    Modified class - Added FamilyId property - Added argument and assingment to constructor

--- LINQ/TestResults.cs
+++ LINQ/TestResults.cs
@@ -2,11 +2,14 @@
 {
     public class TestResults
     {
-        public TestResults(string id)
+        public TestResults(string id, string familyId)
         {
             Id = id;
+            FamilyId = familyId;
         }
 
         public string Id { get; set; }
+
+        public string FamilyId { get; }
     }
 }
