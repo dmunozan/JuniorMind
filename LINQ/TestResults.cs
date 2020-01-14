b7f6208c02407c185b09b017ca820e76d0cef70e
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-01-14 18:14:41 +0200

    Created Score - Added argument and assingment to constructor

--- LINQ/TestResults.cs
+++ LINQ/TestResults.cs
@@ -2,14 +2,17 @@
 {
     public class TestResults
     {
-        public TestResults(string id, string familyId)
+        public TestResults(string id, string familyId, int score)
         {
             Id = id;
             FamilyId = familyId;
+            Score = score;
         }
 
         public string Id { get; set; }
 
         public string FamilyId { get; set; }
+
+        public int Score { get; }
     }
 }
