a26bf6096f0f58a94a976e6c969f758d966b9a9b
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-02-25 16:40:29 +0200

    Created interface to handle the transfer of data between services

---
+++ Net/ChatCommon/ITransfer.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Sockets;
+
+namespace Common
+{
+    interface ITransfer
+    {
+        public string ReceiveData(Socket socket);
+
+        public void SendData(Socket socket, string text);
+    }
+}
