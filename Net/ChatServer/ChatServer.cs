f46de1a906840e8d09462371ce133f0ad34634df
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-02-25 16:50:20 +0200

    Added ITransfer empty implementation

--- Net/ChatServer/ChatServer.cs
+++ Net/ChatServer/ChatServer.cs
@@ -1,6 +1,18 @@
-﻿namespace ChatServer
+﻿using System.Net.Sockets;
+using Common;
+
+namespace ChatServer
 {
-    public class ChatServer
+    public class ChatServer : ITransfer
     {
+        public string ReceiveData(Socket socket)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SendData(Socket socket, string text)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 }
