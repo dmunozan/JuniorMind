0cfe6bd6bdecaecb661b37054da68a8aaa41b7c5
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-09 18:23:18 +0200

    Modified class - Created AddUser method and added initial implementation

--- Net/ChatServer/ChatServerSide.cs
+++ Net/ChatServer/ChatServerSide.cs
@@ -1,6 +1,14 @@
-﻿namespace ChatServer
+﻿using System;
+
+namespace ChatServer
 {
     public class ChatServerSide
     {
+        public bool AddUser(string user)
+        {
+            Console.WriteLine(user);
+
+            return true;
+        }
     }
 }
