63a6cde50f94dd3cf3d17a1d18aee55809e04cc0
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-09 18:22:47 +0200

    Added test - AddUserWhenNewUserShouldAddUserToHashTableAndReturnTrue

--- Net/ChatServer.Tests/ChatServerSideTests.cs
+++ Net/ChatServer.Tests/ChatServerSideTests.cs
@@ -1,6 +1,15 @@
+using Xunit;
+
 namespace ChatServer.Tests
 {
     public class ChatServerSideTests
     {
+        [Fact]
+        public void AddUserWhenNewUserShouldAddUserToHashTableAndReturnTrue()
+        {
+            ChatServerSide server = new ChatServerSide();
+
+            Assert.True(server.AddUser("newUser"));
+        }
     }
 }
