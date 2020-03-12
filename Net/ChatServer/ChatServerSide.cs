0bcc5e079dba73de8003a4963510f9a5a55a53ee
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-12 17:36:36 +0200

    Modified class - Added socket field - Added constructor accepting no arguments to pass previous tests - Added constructor accepting an ISocket objet to handle the communications - Created Start method and implemented it - Start handles the connection of the socket

--- Net/ChatServer/ChatServerSide.cs
+++ Net/ChatServer/ChatServerSide.cs
@@ -1,11 +1,24 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections;
+using System.Net.Sockets;
 
 namespace ChatServer
 {
     public class ChatServerSide
     {
         private readonly Hashtable users = new Hashtable();
+        private readonly ISocket socket;
+
+        public ChatServerSide()
+        {
+            socket = null;
+        }
+
+        public ChatServerSide(ISocket newSocket)
+        {
+            socket = newSocket;
+        }
 
         public bool AddUser(string user)
         {
@@ -21,6 +34,34 @@ namespace ChatServer
 
             users.Add(user, user);
             return true;
+        }
+
+        public void Start()
+        {
+            string trimmedReceivedData = "";
+            while (trimmedReceivedData != "close server")
+            {
+                Console.WriteLine("Waiting for connections...");
+                socket.Listen(1);
+                ISocket connectedSocket = socket.Accept();
+                Console.WriteLine("Connection accepted.");
+                trimmedReceivedData = connectedSocket.Receive();
+                Console.WriteLine("Closing connection.");
+                try
+                {
+                    connectedSocket.Shutdown(SocketShutdown.Both);
+                    connectedSocket.Close();
+                    connectedSocket.SocketDispose();
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Socket error: " + e.ErrorCode);
+                }
+            }
+
+            Console.WriteLine("Closing server.");
+            socket.Close();
+            socket.SocketDispose();
         }
     }
 }
