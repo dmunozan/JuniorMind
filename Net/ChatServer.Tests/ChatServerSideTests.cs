2991b554051c8668c4975617ab9cd4d06ae5c01a
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-17 20:11:30 +0200

    Added test CheckMessageWhenNoNewUserShouldAddMessageToChatAndSendNewMessages

--- Net/ChatServer.Tests/ChatServerSideTests.cs
+++ Net/ChatServer.Tests/ChatServerSideTests.cs
@@ -69,6 +69,29 @@ namespace ChatServer.Tests
         }
 
         [Fact]
+        public void CheckMessageWhenNoNewUserShouldAddMessageToChatAndSendNewMessages()
+        {
+            MockSocketCommunication mockSocket = new MockSocketCommunication();
+
+            ChatServerSide server = new ChatServerSide(mockSocket);
+
+            mockSocket.SentMessages.Add("Initial message");
+
+            string trimmedReceivedData = "userName<sep>sentMessage<sep>lastMessageReceived";
+
+            server.AddUser("userName");
+
+            Assert.False(server.IsNewUser("userName"));
+            Assert.Equal(3, trimmedReceivedData.Split("<sep>").Length);
+            Assert.Equal("sentMessage", server.CheckMessage(trimmedReceivedData));
+            Assert.False(server.IsNewUser("userName"));
+
+            Assert.Collection(mockSocket.SentMessages,
+                item => Assert.Equal("Initial message", item),
+                item => Assert.Equal("userName: sentMessage", item));
+        }
+
+        [Fact]
         public void IsNewUserWhenUserNoExistShouldReturnTrue()
         {
             ChatServerSide server = new ChatServerSide();
