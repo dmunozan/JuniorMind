5620b92e171f8021967eb30367789ab92630a4cb
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-31 18:01:16 +0300

    Modified class - Created LogOn and added initial implementation - Created constructor - Created socket field - Created helper method ReadData

--- Net/ChatClient/ChatClientSide.cs
+++ Net/ChatClient/ChatClientSide.cs
@@ -1,6 +1,34 @@
-﻿namespace ChatClient
+﻿using Common;
+using System;
+
+namespace ChatClient
 {
     public class ChatClientSide
     {
+        private readonly ISocket socket;
+
+        public ChatClientSide(ISocket newSocket)
+        {
+            socket = newSocket;
+        }
+
+        public string LogOn(string userName = null)
+        {
+            userName ??= ReadData("Introduce your user name:");
+
+            socket.Send(userName + "<sep>logon<sep>NoLastMessage");
+
+            string serverReply = socket.Receive();
+
+            Console.WriteLine(serverReply);
+
+            return userName;
+        }
+
+        private string ReadData(string textToShow)
+        {
+            Console.WriteLine(textToShow);
+            return Console.ReadLine();
+        }
     }
 }
