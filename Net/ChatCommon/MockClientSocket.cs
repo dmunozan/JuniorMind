7c597ee2d07c6a354ff392ab7ae20d3fd9782f24
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-03-31 17:55:07 +0300

    Created MockClientSocket - Mock class to simulate the communication with the server

---
+++ Net/ChatCommon/MockClientSocket.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Net.Sockets;
+
+namespace Common
+{
+    public class MockClientSocket : ISocket
+    {
+        public List<string> SentMessages = new List<string>();
+
+        public string TextToReceive { get; set; }
+
+        public ISocket Accept()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Close()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Listen(int backlog)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string Receive()
+        {
+            return TextToReceive;
+        }
+
+        public void Send(string data)
+        {
+            SentMessages.Add(data);
+        }
+
+        public void Shutdown(SocketShutdown how)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void SocketDispose()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
