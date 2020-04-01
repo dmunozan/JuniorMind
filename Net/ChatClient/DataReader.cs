a0ba35aea1bed6e0debe21f6f57567e898c33bfc
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-04-01 18:11:01 +0300

    Created DataReader including the method existing on ChatClientSide

---
+++ Net/ChatClient/DataReader.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ChatClient
+{
+    public class DataReader : IReader
+    {
+        public string Read(string textToShow)
+        {
+            Console.WriteLine(textToShow);
+            return Console.ReadLine();
+        }
+    }
+}
