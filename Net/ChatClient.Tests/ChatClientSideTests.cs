e872e0f36aad4f7fa08bc7bc2f35346cb72c022b
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-04-02 18:18:59 +0300

    Added test - LogOnWhenNullStringShouldSendNothingShowErrorMessageAndRequestForNewUserName

--- Net/ChatClient.Tests/ChatClientSideTests.cs
+++ Net/ChatClient.Tests/ChatClientSideTests.cs
@@ -48,6 +48,26 @@ namespace ChatClient.Tests
         }
 
         [Fact]
+        public void LogOnWhenNullStringShouldSendNothingShowErrorMessageAndRequestForNewUserName()
+        {
+            MockClientSocket mockSocket = new MockClientSocket();
+
+            mockSocket.ListToReceive.Add("server: userName joined the chat.");
+
+            MockDataReader dataReader = new MockDataReader();
+
+            dataReader.ListToRead.Add(null);
+            dataReader.ListToRead.Add("userName");
+
+            ChatClientSide client = new ChatClientSide(mockSocket, dataReader);
+
+            Assert.Equal("userName", client.LogOn());
+
+            Assert.Collection(mockSocket.SentMessages,
+                item => Assert.Equal("userName<sep>logon<sep>NoLastMessage", item));
+        }
+
+        [Fact]
         public void LogOnWhenValidUserNameShouldLogOnAndReceiveGreetingMessage()
         {
             MockClientSocket mockSocket = new MockClientSocket();
