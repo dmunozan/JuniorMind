8c5637385c2db8992fe084865018433d3e8d6a4c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-04-02 18:15:52 +0300

    Added test - LogOnWhenEmptyStringShouldSendNothingShowErrorMessageAndRequestForNewUserName

--- Net/ChatClient.Tests/ChatClientSideTests.cs
+++ Net/ChatClient.Tests/ChatClientSideTests.cs
@@ -6,25 +6,6 @@ namespace ChatClient.Tests
     public class ChatClientSideTests
     {
         [Fact]
-        public void LogOnWhenValidUserNameShouldLogOnAndReceiveGreetingMessage()
-        {
-            MockClientSocket mockSocket = new MockClientSocket();
-
-            mockSocket.ListToReceive.Add("server: userName joined the chat.");
-
-            MockDataReader dataReader = new MockDataReader();
-
-            dataReader.ListToRead.Add("userName");
-
-            ChatClientSide client = new ChatClientSide(mockSocket, dataReader);
-
-            Assert.Equal("userName", client.LogOn());
-
-            Assert.Collection(mockSocket.SentMessages,
-                item => Assert.Equal("userName<sep>logon<sep>NoLastMessage", item));
-        }
-
-        [Fact]
         public void LogOnWhenAlreadyExistingUserNameShouldReceiveErrorMessageAndRequestForNewUserName()
         {
             MockClientSocket mockSocket = new MockClientSocket();
@@ -44,6 +25,45 @@ namespace ChatClient.Tests
             Assert.Collection(mockSocket.SentMessages,
                 item => Assert.Equal("userName<sep>logon<sep>NoLastMessage", item),
                 item => Assert.Equal("newUserName<sep>logon<sep>NoLastMessage", item));
+        }
+
+        [Fact]
+        public void LogOnWhenEmptyStringShouldSendNothingShowErrorMessageAndRequestForNewUserName()
+        {
+            MockClientSocket mockSocket = new MockClientSocket();
+
+            mockSocket.ListToReceive.Add("server: userName joined the chat.");
+
+            MockDataReader dataReader = new MockDataReader();
+
+            dataReader.ListToRead.Add("");
+            dataReader.ListToRead.Add("userName");
+
+            ChatClientSide client = new ChatClientSide(mockSocket, dataReader);
+
+            Assert.Equal("userName", client.LogOn());
+
+            Assert.Collection(mockSocket.SentMessages,
+                item => Assert.Equal("userName<sep>logon<sep>NoLastMessage", item));
+        }
+
+        [Fact]
+        public void LogOnWhenValidUserNameShouldLogOnAndReceiveGreetingMessage()
+        {
+            MockClientSocket mockSocket = new MockClientSocket();
+
+            mockSocket.ListToReceive.Add("server: userName joined the chat.");
+
+            MockDataReader dataReader = new MockDataReader();
+
+            dataReader.ListToRead.Add("userName");
+
+            ChatClientSide client = new ChatClientSide(mockSocket, dataReader);
+
+            Assert.Equal("userName", client.LogOn());
+
+            Assert.Collection(mockSocket.SentMessages,
+                item => Assert.Equal("userName<sep>logon<sep>NoLastMessage", item));
         }
     }
 }
