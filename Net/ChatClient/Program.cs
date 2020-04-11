11cdd0886d849e47f5025407fff003d914e7dfce
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-04-11 16:58:41 +0300

    Modified class - Added implementation to start the client

--- Net/ChatClient/Program.cs
+++ Net/ChatClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 
 namespace ChatClient
 {
@@ -7,6 +8,14 @@ namespace ChatClient
         static void Main()
         {
             Console.WriteLine("Entry point");
+
+            SocketCommunication socket = new SocketCommunication("client");
+
+            DataReader dataReader = new DataReader();
+
+            ChatClientSide client = new ChatClientSide(socket, dataReader);
+
+            client.Start();
         }
     }
 }
