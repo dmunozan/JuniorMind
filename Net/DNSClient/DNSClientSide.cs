967c02956d2eb69f98ffebd28ee7c308a28009e4
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-05-21 17:52:38 +0300

    Created GetIP - Added initial implementation

--- Net/DNSClient/DNSClientSide.cs
+++ Net/DNSClient/DNSClientSide.cs
@@ -1,6 +1,24 @@
-﻿namespace DNSClient
+﻿using System.Net;
+
+namespace DNSClient
 {
     public class DNSClientSide
     {
+        public string GetIP(string hostName)
+        {
+            string ip = null;
+
+            foreach (var address in Dns.GetHostAddresses(hostName))
+            {
+                ip = address.ToString();
+
+                if (ip != null)
+                {
+                    break;
+                }
+            }
+
+            return ip;
+        }
     }
 }
