a6b3606f2e0fe5804dfc4d622b13147e8db3c3c2
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-05-21 17:52:10 +0300

    Added test GetIPWhenValidHostShouldReturnFirstValidAddressFromAdressList

--- Net/DNSClient.Tests/DNSClientSideTests.cs
+++ Net/DNSClient.Tests/DNSClientSideTests.cs
@@ -1,6 +1,33 @@
+using Xunit;
+
 namespace DNSClient.Tests
 {
     public class DNSClientSideTests
     {
+        [Fact]
+        public void GetIPWhenValidHostShouldReturnFirstValidAddressFromAdressList()
+        {
+            DNSClientSide dnsClient = new DNSClientSide();
+
+            string hostIP = dnsClient.GetIP("www.google.com");
+
+            string[] ipOctets = hostIP.Split('.');
+
+            Assert.Equal(4, ipOctets.Length);
+
+            bool validIP = true;
+
+            foreach (var octet in ipOctets)
+            {
+                validIP = byte.TryParse(octet, out byte result);
+
+                if (!validIP)
+                {
+                    break;
+                }
+            }
+
+            Assert.True(validIP);
+        }
     }
 }
