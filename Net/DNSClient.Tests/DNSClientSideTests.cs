d7b4ccfaa0e18db082e008e9907dd53fa61a5f3c
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-05-25 16:42:57 +0300

    Added already passing test GetIPWhenEmptyStringShouldReturnFirstValidAddressFromHostRunningClient

--- Net/DNSClient.Tests/DNSClientSideTests.cs
+++ Net/DNSClient.Tests/DNSClientSideTests.cs
@@ -6,6 +6,17 @@ namespace DNSClient.Tests
     public class DNSClientSideTests
     {
         [Fact]
+        public void GetIPWhenEmptyStringShouldReturnFirstValidAddressFromHostRunningClient()
+        {
+            DNSClientSide dnsClient = new DNSClientSide();
+
+            string hostIP = dnsClient.GetIP("");
+
+            Assert.True(IPAddress.TryParse(hostIP, out IPAddress address));
+            Assert.Contains<IPAddress>(address, Dns.GetHostAddresses(Dns.GetHostName()));
+        }
+
+        [Fact]
         public void GetIPWhenValidHostShouldReturnFirstValidAddressFromAdressList()
         {
             DNSClientSide dnsClient = new DNSClientSide();
