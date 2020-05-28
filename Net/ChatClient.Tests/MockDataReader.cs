939cef801efde4f8c41bcf5ca2b547ec77fa4dc8
Daniel Muñoz Angaril <danielmunozangaril@gmail.com>
2020-05-28 18:04:19 +0300

    Class moved - Moved from ChatClient namespace as it's only used for testing purposes

---
+++ Net/ChatClient.Tests/MockDataReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace ChatClient.Tests
+{
+    class MockDataReader : IReader
+    {
+        public List<string> ListToRead = new List<string>();
+        private int index;
+
+        public string Read(string textToShow)
+        {
+            return ListToRead[index++];
+        }
+    }
+}
